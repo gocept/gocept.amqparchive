c0ac4bc9e91fa14df5aabd6c001b15e8c7ffbbff
Wolfgang Schnerring <wosc@wosc.de>
2011-07-18 13:21:27 +0200

    Re #9345: Introduce helper method for tests that indexes and makes it searchable immediately

--- src/gocept/amqparchive/connection.py
+++ src/gocept/amqparchive/connection.py
@@ -8,7 +8,20 @@ import zope.component
 import zope.interface
 
 
-@zope.interface.implementer(gocept.amqparchive.interfaces.IElasticSearch)
-def ElasticSearch():
-    settings = zope.component.getUtility(gocept.amqprun.interfaces.ISettings)
-    return pyes.ES(settings['gocept.amqparchive.elastic_hostname'])
+class ElasticSearch(pyes.ES):
+
+    zope.interface.implements(gocept.amqparchive.interfaces.IElasticSearch)
+
+    def __init__(self):
+        settings = zope.component.getUtility(
+            gocept.amqprun.interfaces.ISettings)
+        return super(ElasticSearch, self).__init__(
+            settings['gocept.amqparchive.elastic_hostname'])
+
+    def index_immediately(self, *args, **kw):
+        """ElasticSearch by default doesn't update itself immediately,
+        so a newly indexed document is searchable only after a short delay.
+        This method helps avoiding littering the tests with time.sleep(1)
+        """
+        kw.setdefault('querystring_args', {})['refresh'] = True
+        return super(ElasticSearch, self).index(*args, **kw)
