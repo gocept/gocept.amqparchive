2f663adcd662b10083b4924e9cc98e4a9a928d67
Wolfgang Schnerring <ws@gocept.com>
2011-07-20 12:54:37 +0200

    Re #9331: Implement very basic search form

--- src/gocept/amqparchive/browser/js/search.js
+++ src/gocept/amqparchive/browser/js/search.js
@@ -1,18 +1,66 @@
-(function($) {
+// Copyright (c) 2011 gocept gmbh & co. kg
+// See also LICENSE.txt
 
-var elasticsearch = new ElasticSearch({url: '/elasticsearch'});
+// require('jquery')
+// require('jsclass')
+// require('json-template')
+// require('elasticsearch')
+
+
+(function($) {
+gocept = window.gocept || {};
+gocept.amqparchive = gocept.amqparchive || {};
+
+
+gocept.amqparchive.ElasticSearch = Class.extend({
+
+    __init__: function(url) {
+        var self = this;
+        self.elasticsearch = new ElasticSearch({url: url});
+    },
+
+    search: function(query, callback) {
+        var self = this;
+        self.elasticsearch.search({
+            indices: 'queue',
+            types: 'message',
+            queryDSL: {query: {'text': {'_all': query}}},
+            callback: function(json, xhr) {
+                // XXX error handling
+                callback(json);
+            }
+        });
+    }
+
+});
+
+
+gocept.amqparchive.ES = new gocept.amqparchive.ElasticSearch('/elasticsearch');
+
+
+gocept.amqparchive.create_template = function(contents) {
+    return jsontemplate.Template(contents, {
+        default_formatter: 'html'
+    });
+};
+
+
+var RESULT_TEMPLATE = gocept.amqparchive.create_template(
+    '{.repeated section @}<li><a href="/messages/{_source.url}">'
+    + '/messages/{_source.url}</a></li>{.end}'
+);
+
+
+gocept.amqparchive.run_search = function() {
+    gocept.amqparchive.ES.search($('#query').val(), function(data) {
+        $('#results').html(RESULT_TEMPLATE.expand(data.hits.hits));
+    });
+};
+
 
 $(window).bind('load', function() {
-    elasticsearch.search({
-        queryDSL: {query: {'text': {'_all': 'foo'}}},
-        indices: 'queue',
-        types: 'message',
-        callback: function(json, xhr) {
-            $.each(json.hits.hits, function(i, hit) {
-                $('#results').append($('<li>' + hit._source.url + '</li>'));
-            });
-        }
-    });
+    $('#search').bind('click', gocept.amqparchive.run_search);
+    // XXX also bind #query keydown "return"
 });
 
 })(jQuery);
